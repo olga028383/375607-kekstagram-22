11802ad72499f9656cc77c1ac7de3ea6072c430e
Olga <olga028383@mail.ru>
2021-02-16 22:10:30 +0300

    Отрисовала фото

---
+++ js/draw_photo.js
@@ -0,0 +1,17 @@
+import {photos} from './data.js';
+
+const photoList = document.querySelector('.pictures');
+const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const photoListFragment = document.createDocumentFragment();
+
+photos.forEach((photo) => {
+  const photoElement = photoTemplate.cloneNode(true);
+
+  photoElement.setAttribute('href', photo.url);
+  photoElement.querySelector('.picture__img').setAttribute('src', photo.url);
+  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  photoElement.querySelector('.picture__likes').textContent = photo.comments.length;
+  photoListFragment.appendChild(photoElement);
+});
+
+photoList.appendChild(photoListFragment);
